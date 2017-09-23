9838a70b893128d160e1072ff778081c2ec4bf75
Preston <pbriggs28@users.noreply.github.com>
2017-09-23 08:14:53 -0700

    Added instructions to main class

--- src/com/prestonb/AppRunner.java
+++ src/com/prestonb/AppRunner.java
@@ -14,7 +14,14 @@ public class AppRunner {
 	}
 	
 	public void run() {
-		List<NavigationCommand> navCommands = loadNavCommands();
+		// Switch the comments on these two lines to switch between loading from
+		// raw code (wrong approach) to loading from an input file (correct approach)
+		// You will need to complete the method loadNavCommandsFromFile() for this to work
+		
+		List<NavigationCommand> navCommands = loadNavCommandsFromCode();
+//		List<NavigationCommand> navCommands = loadNavCommandsFromFile();
+		
+		
 		
 		RobotNavigator robotNavigator = new RobotNavigator();
 		robotNavigator.setCommandList(navCommands);
@@ -22,12 +29,30 @@ public class AppRunner {
 		Snake.start(robotNavigator);
 	}
 	
-	private List<NavigationCommand> loadNavCommands() {
+	/**
+	 * This is just a hard coded example to show you how this should work.
+	 * Populate the {@link #loadNavCommandsFromFile()} method and change the
+	 * comments above to get this working from an input file.
+	 * 
+	 * @return
+	 */
+	private List<NavigationCommand> loadNavCommandsFromCode() {
 		List<NavigationCommand> navCmdList = new ArrayList<>();
 		navCmdList.add(new NavigationCommand(Direction.RIGHT, 4));
 		navCmdList.add(new NavigationCommand(Direction.UP, 2));
 		navCmdList.add(new NavigationCommand(Direction.RIGHT, 4));
 		
+		return navCmdList;
+	}
+	
+	/**
+	 * Finish this method so it gets the robot navigation commands from an input
+	 * file.
+	 * 
+	 * @return
+	 */
+	private List<NavigationCommand> loadNavCommandsFromFile() {
+		List<NavigationCommand> navCmdList = new ArrayList<>();
 		return navCmdList;
 	}
 }
